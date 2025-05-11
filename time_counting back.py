e92900173a3ff0ae2669a080b76977264888b2eb
NGUYEN TUAN KIET <kietdlv@gmail.com>
2025-05-11 13:23:34 +0700

    khongco gi dac biet

---
+++ time_counting back.py
@@ -0,0 +1,10 @@
+import time
+#check git
+def time_counting(end, start):
+    for x in range (start, end, -1):
+        print(x)
+        time.sleep(1)
+    print("Happy new year!!")
+
+time_counting(0 ,10)
+
